893e82b04ef36c78a3f50aaf8e33ffff6e3bd3b0
Ingabireo <ingabireo64@gmail.com>
2023-01-04 07:32:05 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,23 @@
-#include "main.h"
-/**
- * _memcpy - -fills memory with another buffer.
- * @dest: source string
- * @src: string
- * @n: length of buffer
- * Return: new string
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		*(dest +i) = *(src + i);
-		i++;
-	}	
-	return (dest);
+#include "main.h"                                                                                                                       
+                                                                                                                                        
+/**                                                                                                                                     
+ * *_memcpy -  copies memory area from src to dest                                                                                      
+ * @dest: destination                                                                                                                   
+ * @src: source                                                                                                                         
+ * @n: max bytes to use                                                                                                                 
+ * Return: dest                                                                                                                         
+ */                                                                                                                                     
+                                                                                                                                        
+                                                                                                                                        
+char *_memcpy(char *dest, char *src, unsigned int n)                                                                                    
+{                                                                                                                                       
+        unsigned int i;                                                                                                                 
+                                                                                                                                        
+        for (i = 0; n > 0 ; i++, n--)                                                                                                   
+        {                                                                                                                               
+                dest[i] = src[i];                                                                                                       
+        }                                                                                                                               
+                                                                                                                                        
+        return (dest);                                                                                                                  
 }
+
